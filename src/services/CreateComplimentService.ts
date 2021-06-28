8516ca6660e3d8d1bdb2bcfd25ecc15c39f9280f
Giovane <giovaneaalmeida27@gmail.com>
2021-06-28 20:39:22 -0300

    feat:enviando email às pessoas que receberam elogios

--- src/services/CreateComplimentService.ts
+++ src/services/CreateComplimentService.ts
@@ -1,5 +1,8 @@
 import { ComplimentsRepositories } from '../repositories/ComplimentsRepository';
+import { UserRepositories } from '../repositories/UsersRepository';
 import { getCustomRepository } from 'typeorm';
+import { email, password, host } from '../../token_key';
+import nodeMailer from 'nodemailer';
 
 interface IComplimentRequest {
     user_sender: string;
@@ -12,11 +15,14 @@ export class CreateComplimentService {
 
     async execute({ user_sender, user_receiver, tag_id, message }: IComplimentRequest) {
         const complimentRepository = getCustomRepository(ComplimentsRepositories);
-        
+        const userRepositories = getCustomRepository(UserRepositories);
+
         if (user_receiver === user_sender) {
             throw new Error("It is not allowed to give yourself a compliment");
         }
-        const receiver = await complimentRepository.findByIds([user_receiver]);
+        const receiver = await userRepositories.findOne({
+            id: user_receiver
+        });
 
         if (!receiver) {
             throw new Error("The user you want to compliment does not exist");
@@ -31,6 +37,30 @@ export class CreateComplimentService {
 
         await complimentRepository.save(newCompliment);
 
+        var transporter = nodeMailer.createTransport({
+            host,
+            port: 587,
+            secure:false,
+            requireTLS:true,
+            auth: { user: email, pass: password }
+        });
+          
+        var mailMessage = {
+            from: email,
+            to: receiver.email,
+            subject: 'Test Email Subject',
+            text: 'Test email text'
+        };
+        
+        transporter.sendMail(mailMessage)
+        .then(info => {
+            console.log(info)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        ;
+        
         return newCompliment;
     }
 
